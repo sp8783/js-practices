eb2604d5035ae7b1f68101fa16b7740e12bcf34a
sp8783 <p.sp8783@gmail.com>
2024-03-12 16:18:06 +0900

    Change the method of judging empty strings in `output`

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -4,5 +4,7 @@ for (let count = 1; count <= 20; count++) {
   let output = "";
   if (count % 3 == 0) output += "Fizz";
   if (count % 5 == 0) output += "Buzz";
-  console.log(output || String(count));
+
+  if (output === "") output = String(count);
+  console.log(output);
 }
