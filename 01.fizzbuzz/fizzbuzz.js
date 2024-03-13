953b460c0696b5afd4d3ab8945695cb2e4f9e0ab
sp8783 <p.sp8783@gmail.com>
2024-03-13 10:09:43 +0900

    Fix if statement to describe a block

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,8 +2,14 @@
 
 for (let count = 1; count <= 20; count++) {
   let output = "";
-  if (count % 3 === 0) output += "Fizz";
-  if (count % 5 === 0) output += "Buzz";
-  if (output === "") output = String(count);
+  if (count % 3 === 0) {
+    output += "Fizz";
+  }
+  if (count % 5 === 0) {
+    output += "Buzz";
+  }
+  if (output === "") {
+    output = String(count);
+  }
   console.log(output);
 }
