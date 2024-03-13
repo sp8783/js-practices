ece9987a1fb0609ddaa0b64a848a0fc4b23058aa
sp8783 <p.sp8783@gmail.com>
2024-03-13 09:53:58 +0900

    Remove unnecessary blank lines

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -4,7 +4,6 @@ for (let count = 1; count <= 20; count++) {
   let output = "";
   if (count % 3 === 0) output += "Fizz";
   if (count % 5 === 0) output += "Buzz";
-
   if (output === "") output = String(count);
   console.log(output);
 }
