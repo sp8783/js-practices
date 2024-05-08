0f2dbdac2adedee77f0f801cfb778af3ae2e9b3f
sp8783 <p.sp8783@gmail.com>
2024-05-09 03:22:04 +0900

    Remove unnecessary error handling in `callback.js`

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -6,40 +6,21 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   `
-CREATE TABLE books (
+  CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL UNIQUE
-);
-`,
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      db.run(
-        `INSERT INTO books (title) VALUES ('Railsの教科書');`,
-        function (err) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(`ID:${this.lastID}のデータが追加されました`);
-            db.all(`SELECT * FROM books;`, function (err, rows) {
-              if (err) {
-                console.log(err);
-              } else {
-                rows.forEach((row) => {
-                  console.log(`ID:${row.id}, Title: ${row.title}`);
-                });
-                db.run(`DROP TABLE books;`, function (err) {
-                  if (err) {
-                    console.log(err);
-                  }
-                });
-              }
-            });
-          }
-        },
-      );
-    }
+  );
+  `,
+  () => {
+    db.run(`INSERT INTO books (title) VALUES ('Railsの教科書');`, function () {
+      console.log(`ID:${this.lastID}のデータが追加されました`);
+      db.all(`SELECT * FROM books;`, function (_, rows) {
+        rows.forEach((row) => {
+          console.log(`ID:${row.id}, Title: ${row.title}`);
+        });
+        db.run(`DROP TABLE books;`);
+      });
+    });
   },
 );
 
@@ -48,15 +29,15 @@ console.log("▼異常系ログ");
 
 db.run(
   `
-CREATE TABLE books (
+  CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL UNIQUE
-);
-`,
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
+  );
+  `,
+  () => {
+    db.run(`INSERT INTO books (title) VALUES ('Railsの教科書');`, function () {
+      console.log(`ID:${this.lastID}のデータが追加されました`);
+      // ここで一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
       db.run(
         `INSERT INTO books (title) VALUES ('Railsの教科書');`,
         function (err) {
@@ -64,36 +45,20 @@ CREATE TABLE books (
             console.log(err);
           } else {
             console.log(`ID:${this.lastID}のデータが追加されました`);
-            // ここで一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
-            db.run(
-              `INSERT INTO books (title) VALUES ('Railsの教科書');`,
-              function (err) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log(`ID:${this.lastID}のデータが追加されました`);
-                }
-                // ここでレコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
-                db.all(`SELECT * FROM foods;`, function (err, rows) {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    rows.forEach((row) => {
-                      console.log(`ID:${row.id}, Title: ${row.title}`);
-                    });
-                    db.run(`DROP TABLE books;`, function (err) {
-                      if (err) {
-                        console.log(err);
-                      }
-                      db.close();
-                    });
-                  }
-                });
-              },
-            );
           }
+          // ここでレコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
+          db.all(`SELECT * FROM foods;`, function (err, rows) {
+            if (err) {
+              console.log(err);
+            } else {
+              rows.forEach((row) => {
+                console.log(`ID:${row.id}, Title: ${row.title}`);
+              });
+            }
+            db.run(`DROP TABLE books;`);
+          });
         },
       );
-    }
+    });
   },
 );
