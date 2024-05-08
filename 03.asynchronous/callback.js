eb5f064ce8132ccd56a51bac6816411d410e8265
sp8783 <p.sp8783@gmail.com>
2024-05-08 21:16:46 +0900

    Add callback-style asynchronous processing (exception scenario)

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 console.log("▼正常系ログ");
 const db = new sqlite3.Database(":memory:");
@@ -32,10 +33,64 @@ CREATE TABLE books (
                   if (err) {
                     console.log(err);
                   }
-                  db.close();
                 });
               }
             });
+          }
+        },
+      );
+    }
+  },
+);
+
+await timers.setTimeout(100);
+console.log("▼異常系ログ");
+
+db.run(
+  `
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+);
+`,
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      db.run(
+        `INSERT INTO books (title) VALUES ('Railsの教科書');`,
+        function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`ID:${this.lastID}のデータが追加されました`);
+            // ここで一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
+            db.run(
+              `INSERT INTO books (title) VALUES ('Railsの教科書');`,
+              function (err) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(`ID:${this.lastID}のデータが追加されました`);
+                }
+                // ここでレコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
+                db.all(`SELECT * FROM foods;`, function (err, rows) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    rows.forEach((row) => {
+                      console.log(`ID:${row.id}, Title: ${row.title}`);
+                    });
+                    db.run(`DROP TABLE books;`, function (err) {
+                      if (err) {
+                        console.log(err);
+                      }
+                      db.close();
+                    });
+                  }
+                });
+              },
+            );
           }
         },
       );
