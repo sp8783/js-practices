2b7d9e776062faad7fcbc8e367beccff7a0309a9
sp8783 <p.sp8783@gmail.com>
2024-06-19 13:30:09 +0900

    Fix `promise.js` and `async/await.js` style and process to match `callback.js`

--- 03.asynchronous/async-await.js
+++ 03.asynchronous/async-await.js
@@ -2,9 +2,9 @@ import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 import { run, all } from "./sqlite3-promise-wrapper.js";
 
+console.log("▼正常系ログ");
 const db = new sqlite3.Database(":memory:");
 
-console.log("▼正常系ログ");
 await run(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
@@ -16,12 +16,14 @@ const result = await run(
 );
 console.log(`ID:${result.lastID}のデータが追加されました`);
 const rows = await all(db, "SELECT * FROM books;");
-rows.forEach((row) => console.log(`ID:${row.id}, Title:${row.title}`));
+rows.forEach((row) => {
+  console.log(`ID:${row.id}, Title:${row.title}`);
+});
 await run(db, "DROP TABLE books;");
 
 await timers.setTimeout(100);
-
 console.log("▼異常系ログ");
+
 await run(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
@@ -33,14 +35,22 @@ const result2 = await run(
 );
 console.log(`ID:${result2.lastID}のデータが追加されました`);
 try {
-  await run(db, "INSERT INTO books (title) VALUES (?);", "Railsの教科書"); // 一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
+  const result3 = await run(
+    db,
+    "INSERT INTO books (title) VALUES (?);",
+    "Railsの教科書",
+  ); // ここで一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
+  console.log(`ID:${result3.lastID}のデータが追加されました`);
 } catch (err) {
   if (err.code === "SQLITE_CONSTRAINT") {
     console.error(err.message);
   }
 }
 try {
-  await run(db, "SELECT * FROM foods;"); // レコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
+  const rows = await run(db, "SELECT * FROM foods;"); // ここでレコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
+  rows.forEach((row) => {
+    console.log(`ID:${row.id}, Title:${row.title}`);
+  });
 } catch (err) {
   if (err.code === "SQLITE_ERROR") {
     console.error(err.message);
--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -2,9 +2,9 @@ import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 import { run, all } from "./sqlite3-promise-wrapper.js";
 
+console.log("▼正常系ログ");
 const db = new sqlite3.Database(":memory:");
 
-console.log("▼正常系ログ");
 run(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
@@ -15,13 +15,15 @@ run(
     return all(db, "SELECT * FROM books;");
   })
   .then((rows) =>
-    rows.forEach((row) => console.log(`ID:${row.id}, Title:${row.title}`)),
+    rows.forEach((row) => {
+      console.log(`ID:${row.id}, Title:${row.title}`);
+    }),
   )
   .then(() => run(db, "DROP TABLE books;"));
 
 await timers.setTimeout(100);
-
 console.log("▼異常系ログ");
+
 run(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
@@ -29,14 +31,22 @@ run(
   .then(() => run(db, "INSERT INTO books (title) VALUES (?);", "Railsの教科書"))
   .then((result) => {
     console.log(`ID:${result.lastID}のデータが追加されました`);
-    return run(db, "INSERT INTO books (title) VALUES (?);", "Railsの教科書"); // 一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
+    return run(db, "INSERT INTO books (title) VALUES (?);", "Railsの教科書"); // ここで一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
+  })
+  .then((result) => {
+    console.log(`ID:${result.lastID}のデータが追加されました`);
   })
   .catch((err) => {
     if (err.code === "SQLITE_CONSTRAINT") {
       console.error(err.message);
     }
   })
-  .then(() => run(db, "SELECT * FROM foods;")) // レコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
+  .then(() => run(db, "SELECT * FROM foods;")) // ここでレコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
+  .then((rows) =>
+    rows.forEach((row) => {
+      console.log(`ID:${row.id}, Title:${row.title}`);
+    }),
+  )
   .catch((err) => {
     if (err.code === "SQLITE_ERROR") {
       console.error(err.message);
