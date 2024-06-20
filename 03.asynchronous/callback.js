ec4d9f0c2848eb6261592db5ee1b2cc4d0bb93e2
sp8783 <p.sp8783@gmail.com>
2024-06-20 14:32:04 +0900

    Fix to use double quotes for strings that do not embed variables

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -5,18 +5,18 @@ console.log("▼正常系ログ");
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);`,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
   () => {
     db.run(
-      `INSERT INTO books (title) VALUES (?);`,
+      "INSERT INTO books (title) VALUES (?);",
       "Railsの教科書",
       function () {
         console.log(`ID:${this.lastID}のデータが追加されました`);
-        db.all(`SELECT * FROM books;`, (_, rows) => {
+        db.all("SELECT * FROM books;", (_, rows) => {
           rows.forEach((row) => {
             console.log(`ID:${row.id}, Title:${row.title}`);
           });
-          db.run(`DROP TABLE books;`);
+          db.run("DROP TABLE books;");
         });
       },
     );
@@ -27,16 +27,16 @@ await timers.setTimeout(100);
 console.log("▼異常系ログ");
 
 db.run(
-  `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);`,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
   () => {
     db.run(
-      `INSERT INTO books (title) VALUES (?);`,
+      "INSERT INTO books (title) VALUES (?);",
       "Railsの教科書",
       function () {
         console.log(`ID:${this.lastID}のデータが追加されました`);
         // ここで一意制約エラーを発生させるため、同じタイトルのレコードを挿入する
         db.run(
-          `INSERT INTO books (title) VALUES (?);`,
+          "INSERT INTO books (title) VALUES (?);",
           "Railsの教科書",
           function (err) {
             if (err) {
@@ -47,7 +47,7 @@ db.run(
               console.log(`ID:${this.lastID}のデータが追加されました`);
             }
             // ここでレコード取得のエラーを発生させるため、存在しないテーブル名（foods）を指定する
-            db.all(`SELECT * FROM foods;`, (err, rows) => {
+            db.all("SELECT * FROM foods;", (err, rows) => {
               if (err) {
                 if (err instanceof Error && err.code === "SQLITE_ERROR") {
                   console.error(err.message);
@@ -57,7 +57,7 @@ db.run(
                   console.log(`ID:${row.id}, Title:${row.title}`);
                 });
               }
-              db.run(`DROP TABLE books;`);
+              db.run("DROP TABLE books;");
             });
           },
         );
